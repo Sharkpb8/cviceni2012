a938e5a6e486dbc891ba24167c0047bc15e4f286
sharkpb8 <sharkpb8@gmail.com>
2023-12-20 08:39:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const canvas = document.getElementById('Canva');
+const ctx = canvas.getContext('2d');
+let brushSize = 5;
+let currentColor = '#000';
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function changeColor(event) {
+    currentColor = event.target.value;
+ }
+
+let painting = false;
+
+    function startPosition(e) {
+      painting = true;
+      draw(e);
+    }
+
+    function endPosition() {
+      painting = false;
+      ctx.beginPath();
+    }
+
+    function draw(e) {
+      if (!painting) return;
+
+      ctx.lineWidth = brushSize;
+      ctx.lineCap = 'round';
+      ctx.strokeStyle = currentColor;
+
+      ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.arc(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, brushSize / 2, 0, Math.PI * 2);
+      ctx.fillStyle = currentColor;
+      ctx.fill();
+      ctx.beginPath();
+      ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    }
+
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', endPosition);
+    canvas.addEventListener('mousemove', draw);
